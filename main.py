16977950581865f2f8430fcdd91d7089558f3bf9
ROCCYK <82534598+ROCCYK@users.noreply.github.com>
2021-11-03 11:36:14 -0400

    v1

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ Gas_Fuel_Price = float(input('Gas Price: '))
 
 fuel_cost = Trip_Distance * Fuel_Efficiency * Gas_Fuel_Price
 
-print(f'This trip will require {round((Fuel_Efficiency * 100), 2)} liters of fuel, which amounts to a fuel cost of ${round(fuel_cost,2)}')
+print(f'This trip will require {round((Fuel_Efficiency * Trip_Distance), 2)} liters of fuel, which amounts to a fuel cost of ${round(fuel_cost,2)}')
